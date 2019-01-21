f434ab644b220d5261bd3f4c8da873f37f60f308
Ruslan Malona <rmalyona@smartling.com>
2019-01-21 12:07:08 +0200

    New Polymorphic type `PerformanceCalculator`, move functions `amountFor` and `volumeCreditsFor` into it

--- src/main/java/org/mcvly/refactoring/first_example/StatementData.java
+++ src/main/java/org/mcvly/refactoring/first_example/StatementData.java
@@ -16,11 +16,10 @@ class StatementData {
 
     @Data
     static class Performance {
-        private int audience;
-        private double amount;
-        private int volumeCredits;
-        private String playName;
-        private String playType;
+        private final String playName;
+        private final double amount;
+        private final int audience;
+        private final int volumeCredits;
     }
 
     public StatementData(Map plays, Map invoice) {
@@ -32,15 +31,13 @@ class StatementData {
 
     private List<StatementData.Performance> enrichPerformance(Map plays, List<Map> performances) {
         return performances.stream().map(performanceDTO -> {
-            Map<String, String> play = (Map<String, String>) plays.get(performanceDTO.get("playID"));
-            StatementData.Performance performance = new StatementData.Performance();
-            performance.setAudience((Integer) performanceDTO.get("audience"));
-            performance.setPlayName(play.get("name"));
-            performance.setPlayType(play.get("type"));
-            performance.setAmount(amountFor(performance));
-            performance.setVolumeCredits(volumeCreditsFor(performance));
-            return performance;
+            PerformanceCalculator calculator = new PerformanceCalculator(performanceDTO, playFor(plays, performanceDTO));
+            return new Performance(calculator.getPlayName(), calculator.getAmount(), calculator.getAudience(), calculator.getVolumeCredits());
         }).collect(toList());
+    }
+
+    private Map<String, String> playFor(Map plays, Map performanceDTO) {
+        return (Map<String, String>) plays.get(performanceDTO.get("playID"));
     }
 
 
@@ -50,37 +47,5 @@ class StatementData {
 
     private int totalVolumeCredits() {
         return performances.stream().mapToInt(performance -> performance.volumeCredits).sum();
-    }
-
-    private int volumeCreditsFor(StatementData.Performance performance) {
-        int result = 0;
-        result += Math.max(performance.getAudience() - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy".equals(performance.getPlayType())) {
-            result += Math.floor(performance.getAudience() / 5);
-        }
-        return result;
-    }
-
-    private double amountFor(StatementData.Performance performance) {
-        double result;
-        switch (performance.getPlayType()) {
-            case "tragedy":
-                result = 40000;
-                if (performance.getAudience() > 30) {
-                    result += 1000 * (performance.getAudience() - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (performance.getAudience() > 20) {
-                    result += 10000 + 500 * (performance.getAudience() - 20);
-                }
-                result += 300 * performance.getAudience();
-                break;
-            default:
-                throw new RuntimeException("Unknown type: " + performance.getPlayType());
-        }
-        return result;
     }
 }
